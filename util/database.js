9867b60323d8e0dd7430466fd5b695e091eee833
armit4ge <kelvinquida24@gmail.com>
2022-07-30 23:57:41 -0400

    switching dbs mysql to mongodb (nt)

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'm2433',{
-    dialect: "mysql",
-    host: "localhost",
-})
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://Armit4ge:pAYLgo7QnP01oPCS@cluster0.t3haj.mongodb.net/?retryWrites=true&w=majority')
+    .then(client => {
+        console.log("Connected")
+        callback(client)
+    })
+    .catch(err => console.log(err))
 
-module.exports = sequelize
+}
+
+module.exports = mongoConnect
