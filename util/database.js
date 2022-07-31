56a41be68cb92ccd0187f06911881e143795c54f
armit4ge <kelvinquida24@gmail.com>
2022-07-31 09:31:43 -0400

    testing dotenv

--- util/database.js
+++ util/database.js
@@ -1,10 +1,12 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+require('dotenv').config()
 
 let _db
+let pass = process.env.MY_PASS
 
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://Armit4ge:pAYLgo7QnP01oPCS@cluster0.t3haj.mongodb.net/shop?retryWrites=true&w=majority')
+    MongoClient.connect(`mongodb+srv://Armit4ge:${pass}@cluster0.t3haj.mongodb.net/shop?retryWrites=true&w=majority`)
     .then(client => {
         console.log("Connected")
         _db = client.db()
@@ -22,9 +24,6 @@ const getDb = () => {
     }
     throw "NO DB"
 }
-
-
-
 
 exports.mongoConnect = mongoConnect
 exports.getDb = getDb
