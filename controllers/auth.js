d223030b269514925782f15b04018fa91b8ee080
armit4ge <kelvinquida24@gmail.com>
2022-10-22 22:29:02 -0400

    adding auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,14 @@ export const getLogin = (req, res, next) => {
       isAuthenticated:false
     })
 };
+
+export const getSignup = (req,res) => {
+  res.render('auth/signup',{
+    path:'/signup',
+    pageTitle:'Signup',
+    isAuthenticated:false
+  })
+}
    
 export const postLogin = (req, res, next) => {
   User.findById('62fcde8f985a7b4566a06edd')
@@ -21,11 +29,15 @@ export const postLogin = (req, res, next) => {
       .catch(err => console.log(err))
 };
 
+export const postSignup = (req,res) => {
+
+}
+
 export const postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err)
     res.redirect('/')
   })
 };
- 
+
  
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 import {Router} from 'express'
-import { getLogin,postLogin,postLogout } from '../controllers/auth.js'
+import { getLogin,postLogin,postLogout,getSignup,postSignup } from '../controllers/auth.js'
 
 const router = Router()
 
 router.get('/login',getLogin)
 
+router.get('/signup',getSignup)
+
 router.post('/login',postLogin)
+
+router.post('/signup',postSignup)
 
 router.post('/logout',postLogout)
 
